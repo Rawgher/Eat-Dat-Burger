13ee4b832d4182083b673928acceef3746346519
Roger <rdpender3@gmail.com>
2018-09-09 16:58:31 -0700

    getting things started

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
 let express = require("express");
 let bodyParser = require("body-parser");
+
+// like activity 17 catsapp
+// make the page not refresh
+// be mobile responsive
+// add a favicon
+// used ajax rather than post page refreshes for moving the burgers to the devoured spot
