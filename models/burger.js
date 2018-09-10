ee1361834e038a553a235759b45f7da6cb4e0b57
Roger <rdpender3@gmail.com>
2018-09-09 17:34:01 -0700

    finished burger.js?

---
+++ models/burger.js
@@ -0,0 +1,32 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+  selectAll: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
+
+// saving for if add functionality from orm file
+// ,
+//   delete: function(condition, cb) {
+//     orm.delete("burgers", condition, function(res) {
+//       cb(res);
+//     });
+//   }
