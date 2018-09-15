a59a8c87132aa4190ced9ab6d25571a9f5e183a3
Roger <rdpender3@gmail.com>
2018-09-14 18:01:27 -0700

    adding jawsdb for heroku

--- config/connection.js
+++ config/connection.js
@@ -4,15 +4,21 @@ require("dotenv").config();
 // Set up MySQL connection
 const mysql = require("mysql");
 
-let connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   
-  host: "localhost",
-  port: 3306,
-  user: process.env.MySQL_Username,
-  password: process.env.MySQL_Password,
-  database: "burgers_db"
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.MySQL_Username,
+    password: process.env.MySQL_Password,
+    database: "burgers_db"
+    
+  });
+}
 
-});
+
 
 // Making connection to the database
 connection.connect(function (err) {
